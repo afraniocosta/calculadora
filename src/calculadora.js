bd1ed753b25de8e32cd6a2f44799b025ce7c08a5
afraniocosta <afraniompc@gmail>
2021-02-23 23:26:33 -0300

    interface html - parte 2

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './calculadora.css';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 function Calculadora() {
   return (
@@ -11,7 +11,16 @@ function Calculadora() {
       margin: '5px',
       width: '400px'
     }}>
-
+      <Container>
+        <Row>
+          <Col xs="3">
+            <Button variant='danger'>C</Button>
+          </Col>
+          <Col xs="9">
+            <Form.Control type='text' name='textNumeros' class='text-right' readOnly='readonly' />
+          </Col>
+        </Row>
+      </Container>
     </Jumbotron>
   );
 }
