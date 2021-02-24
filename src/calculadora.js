29b65383ce179fb694f64dae7d9f54ef53561813
afraniocosta <afraniompc@gmail>
2021-02-23 23:16:03 -0300

    interface html

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './calculadora.css';
+import { Jumbotron } from 'react-bootstrap';
 
 function Calculadora() {
   return (
-    <h1>Calculadora</h1>
+    <Jumbotron style={{
+      background: 'transparent !important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px'
+    }}>
+
+    </Jumbotron>
   );
 }
 
